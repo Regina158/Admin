d0d3d6a6a5abf938c269f02b012ff6f1f26276a2
regina158 <reginantx5@gmail.com>
2024-06-28 20:39:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import DashboardAdmin from "./pages/DashboardAdmin";
+import DashboardContent from "./components/DashboardContent";
+import ArtikelContent from "./components/ArtikelContent";
+import CreateArtikel from "./components/CreateArtikel";
+import EditArtikel from "./components/EditArtikel";
+import PesanContent from "./components/PesanContent";
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<DashboardAdmin />}>
+          <Route index element={<DashboardContent />} />
+          <Route path="artikel" element={<ArtikelContent />} />
+          <Route path="artikel/create" element={<CreateArtikel />} />
+          <Route path="artikel/edit/:id" element={<EditArtikel />} />
+          <Route path="pesan" element={<PesanContent />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
